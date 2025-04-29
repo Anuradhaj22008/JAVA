7dee0d8cc4b64793f89f73f1fc47d57d8771cb1e
Anuradha Jadhav <164736052+Anuradhaj22008@users.noreply.github.com>
2025-04-29 23:41:58 +0530

    Add files via upload

---
+++ midstar.java
@@ -0,0 +1,19 @@
+package pattern;
+
+public class midstar {
+    public static void main(String[] args){
+        //output
+        System.out.println(" * * * * * ");
+        System.out.println(" * * * * ");
+        System.out.println(" * * * ");
+        System.out.println(" * * ");
+        System.out.println(" * *");
+        System.out.println(" * ");
+        System.out.println(" * * ");
+        System.out.println(" * * *  ");
+        System.out.println(" * * * * ");
+        System.out.println(" * * * * * ");
+        
+    }
+    
+}
