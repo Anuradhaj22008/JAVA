3b5e50dcfe33f498b64971ec2e2e7de575647d1f
Anuradha Jadhav <164736052+Anuradhaj22008@users.noreply.github.com>
2025-04-30 00:10:16 +0530

    Add files via upload

---
+++ incr.java
@@ -0,0 +1,12 @@
+package pattern;
+
+public class incr {
+    public static void main(String[] args) {
+        //output
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * * ");
+        System.out.println("* * * * ");
+        System.out.println("* * * * * ");
+    }
+}
