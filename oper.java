1392d3e4a053b88abf0aeda944b1bb8908bcf79a
Anuradha Jadhav <164736052+Anuradhaj22008@users.noreply.github.com>
2025-05-03 00:09:43 +0530

    Add files via upload

---
+++ oper.java
@@ -0,0 +1,18 @@
+package arth;
+
+public class oper {
+    public static void main(String[] args) {
+        //output
+        int a=4;
+        int b=2;
+        int sum;
+        int mul;
+        sum=a+b;
+        System.out.print("The addition of a and b is ");
+        System.out.println(sum);      //mul
+        mul=a*b;
+        System.out.print("The multiplication of a and b is ");
+        System.out.println(mul);
+    }
+    
+}
