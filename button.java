6f2ace5f1157b5d7d133e9b2f80e72fd0713829a
Anuradha Jadhav <164736052+Anuradhaj22008@users.noreply.github.com>
2025-05-04 22:34:10 +0530

    Add files via upload

---
+++ button.java
@@ -0,0 +1,27 @@
+package button;
+
+import java.util.*;
+
+public class button {  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+        //output
+        switch (button) {
+            case 1:
+                System.out.println("hello");
+                break;
+            case 2:
+                System.out.println("hi");
+                break;
+            case 3:
+                System.out.println("hey");
+                break;
+        
+            default:
+                System.out.println("the number is not 1,2 and 3");
+                break;
+        }
+    }
+    
+}
