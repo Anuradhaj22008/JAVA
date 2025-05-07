2e6d5789e1e27d0ed5a9a2ea69bebdecc9425e22
Anuradha Jadhav <164736052+Anuradhaj22008@users.noreply.github.com>
2025-05-07 23:07:45 +0530

    Add files via upload

---
+++ whileex.java
@@ -0,0 +1,12 @@
+package loopcondition;
+
+public class whileex {
+    public static void main(String[] args) {
+        int i=0;
+        while(i<1){
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
