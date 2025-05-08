f3bfaf6beaead5450ea3e3cf128dca710e037b86
Anuradha Jadhav <164736052+Anuradhaj22008@users.noreply.github.com>
2025-05-09 00:01:21 +0530

    Add files via upload

--- forex.java
+++ forex.java
@@ -3,9 +3,8 @@ package loopcondition;
 public class forex {
     public static void main(String[] args) {
         //for loop
-        for (int i=0; i<100; i=i+1) {
-            System.out.println("Hello World ");
-        }
+        for (int i=0; i<=100; i++) {
+        System.out.println(i);}
     }
     
 }
