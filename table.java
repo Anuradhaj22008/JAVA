e3124395cad78c3b9b348be279305af8d60a5f25
Anuradha Jadhav <164736052+Anuradhaj22008@users.noreply.github.com>
2025-05-08 23:58:24 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,13 @@
+package loopcondition;
+import java.util.*;
+public class table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to print table: ");
+        int n = sc.nextInt();
+        for(int i=1; i<=10; i++){
+            System.out.println(n*i);
+        }
+    }
+    
+}
