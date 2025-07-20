dcdb67eeac901b185dce53a17ec85b2aa739761e
Anuradha Jadhav <164736052+Anuradhaj22008@users.noreply.github.com>
2025-07-20 23:58:12 +0530

    Add files via upload

---
+++ addit.java
@@ -0,0 +1,23 @@
+package fun;// always above on import statements
+import java.util.*;// for the scanner class
+
+
+//making a function to add two numbers
+public class addit {
+    public static int add(int a,int b){
+        int c=a+b;
+        return c;
+
+    }
+    //main function
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = add(a ,b);
+        System.out.println(c);// printing the sum of two numbers
+
+
+    }
+    
+}
